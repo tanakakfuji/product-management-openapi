b3066dc3d8d6c8c6fc12940f20fe28b575dda394
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2026-06-04 15:41:57 +0900

    orvalでOpenAPI仕様書から自動生成

---
+++ web/orval.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "orval";
+export default defineConfig({
+  petstore: {
+    input: "../api/build/openapi.json",
+    output: {
+      target: "./src/api/api.ts",
+      schemas: "./src/api/model",
+      client: "react-query",
+      baseUrl: "http://localhost:8080",
+    },
+  },
+});
